14ebac9a9bb8d3de9dc96be717710dd4e3841c65
Sherlline-Josephin <109158067+Sherlline-Josephin@users.noreply.github.com>
2023-03-12 20:57:38 +0530

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,4 @@
-import aboutMeImg from "../images/ME.jpg";
+import aboutMeImg from "../images/lg.png";
 import { motion } from "framer-motion";
 import SocialIcons from "../components/SocialIcons";
 import { useInView } from "react-intersection-observer";
